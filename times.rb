837731556d95970fb10a6b743b55f48cb54e51cc
Huda Yousif <hjdoski@gmail.com>
2019-10-08 23:48:17 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,11 @@
 def using_times
-	#your code here
+	counter = 0 
+
+7.times do	
+	puts "Wingardim Leviosa"
 end
 
 
 
 
+Fill out the content of the using_times method so that calling it will puts the desired phrase, "Wingardium Leviosa" seven times by using the times keyword.
